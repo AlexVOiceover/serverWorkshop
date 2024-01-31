716359412f3a01c84a6fb93639bd3f0e5fe51352
AlexVOiceover <alex@alexvoiceover.com>
2024-01-31 12:40:22 +0000

    Finito

--- model.js
+++ model.js
@@ -1,49 +1,61 @@
-const db = require("./database/db.js");
+const db = require('./database/db.js')
 
 const select_cohorts_in_finsbo = db.prepare(/*sql*/ `
-  -- [1]
-`);
+  SELECT name FROM cohorts WHERE location = 'Finsbury Park'
+`)
 
 function listCohortsInFinsbo() {
-  return select_cohorts_in_finsbo.all();
+	return select_cohorts_in_finsbo.all()
 }
 
 const select_students_in_finsbo = db.prepare(/*sql*/ `
-  -- [2]
-`);
+  SELECT username FROM students JOIN cohorts ON students.cohort_name = cohorts.name WHERE cohorts.location = 'Finsbury Park'
+`)
 
 function listStudentsInFinsbo() {
-  return select_students_in_finsbo.all();
+	return select_students_in_finsbo.all()
 }
 
 const select_students_with_location = db.prepare(/*sql*/ `
-  -- [3]
-`);
+  SELECT username, location FROM students JOIN cohorts ON students.cohort_name = cohorts.name
+`)
 
 function listStudentsWithLocation() {
-  return select_students_with_location.all();
+	return select_students_with_location.all()
 }
 
 const select_students_with_projects = db.prepare(/*sql*/ `
-  -- [4]
-`);
+  SELECT projects.name, students_projects.student_username AS username
+  FROM students_projects JOIN projects
+    ON students_projects.project_id = projects.id
+`)
 
 function listStudentsWithProjects() {
-  return select_students_with_projects.all();
+	return select_students_with_projects.all()
 }
 
+//List all project names with the usernames of the students who worked
+//on them, only for students who attended FAC in Finsbury Park.
 const select_students_with_projects_in_finsbo = db.prepare(/*sql*/ `
-  -- [5]
-`);
+  SELECT projects.name, students.username
+  FROM projects
+  JOIN students_projects ON students_projects.project_id = projects.id
+  JOIN students ON students_projects.student_username = students.username
+  JOIN cohorts ON students.cohort_name = cohorts.name
+   
+  WHERE cohorts.location = 'Finsbury Park'
+
+  ORDER BY student_username
+`)
 
 function listStudentsWithProjectsInFinsbo() {
-  return select_students_with_projects_in_finsbo.all();
+	return select_students_with_projects_in_finsbo.all()
 }
 
 module.exports = {
-  listCohortsInFinsbo,
-  listStudentsInFinsbo,
-  listStudentsWithLocation,
-  listStudentsWithProjects,
-  listStudentsWithProjectsInFinsbo,
-};
+	listCohortsInFinsbo,
+	listStudentsInFinsbo,
+	listStudentsWithLocation,
+	listStudentsWithProjects,
+	listStudentsWithProjectsInFinsbo,
+}
